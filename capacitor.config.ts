9da3906d8e302ed0f723cffa2f8b265196a95559
r1cheX <rbraulporras@gmail.com>
2022-12-15 21:34:47 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sistventas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
